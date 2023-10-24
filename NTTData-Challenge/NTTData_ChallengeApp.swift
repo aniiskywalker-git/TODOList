4223d3ab10abb06502796fd98b264ce96a0ffe05
aniiskywalker-git <anavictoria.frias.25@outlook.com>
2023-10-23 19:25:20 -0600

    Initial Commit

---
+++ NTTData-Challenge/NTTData_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTTData_ChallengeApp.swift
+//  NTTData-Challenge
+//
+//  Created by Ana Victoria Frias on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NTTData_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
